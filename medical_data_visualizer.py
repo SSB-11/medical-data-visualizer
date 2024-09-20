1849382321b24debb2e7eef9da5e8741e42f4953
SSB-11 <shelles.borges@gmail.com>
2024-09-20 19:21:13 +0000

    feat: add overweight column and normalize data

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,13 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+def calculate_bmi(cm_height, kg_weight):
+    bmi = kg_weight/pow(cm_height/100, 2) # Height in centimeters to meters
+    return round(bmi, 1)
+
+df['overweight'] = np.where(calculate_bmi(df['height'], df['weight']) > 25, 1, 0)
 
 # 3
-
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
